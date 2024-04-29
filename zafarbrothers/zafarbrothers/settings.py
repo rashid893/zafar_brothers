5013fb18340e9b4feaf5b52efdf2cb8de0609d56
rashid <rbrohi96@gmail.com>
2024-04-29 11:18:50 +0500

    ass

--- zafarbrothers/zafarbrothers/settings.py
+++ zafarbrothers/zafarbrothers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xveht*3y2=lncn%2ie-(n0!(5xp_z2&9-e_9-i@=0vsr$it*jc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','85.239.241.96']
 
 
 # Application definition
